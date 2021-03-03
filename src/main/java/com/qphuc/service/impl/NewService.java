27345c722dff2622eb4594134612249cfd7d113c
Qphuc <tqp1610@gmail.com>
2021-03-03 09:35:14 +0700

    add news

--- src/main/java/com/qphuc/service/impl/NewService.java
+++ src/main/java/com/qphuc/service/impl/NewService.java
@@ -1,22 +1,32 @@
 package com.qphuc.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.qphuc.dao.INewDAO;
+import com.qphuc.entity.NewEntity;
 import com.qphuc.model.NewModel;
+import com.qphuc.repository.NewRepository;
 import com.qphuc.service.INewService;
 
 @Service
 public class NewService implements INewService {
 	
 	@Autowired
-	private INewDAO newDao;
+	private NewRepository newRepository;
 	
 	@Override
 	public List<NewModel> findAll() {
-		return newDao.findAll();
+		List<NewModel> models = new ArrayList<>();
+		List<NewEntity> entities = newRepository.findAll();
+		for(NewEntity item :entities) {
+			NewModel newModel = new NewModel();
+			newModel.setTitle(item.getTitle());
+			newModel.setShortDescription(item.getShortDescription());
+			models.add(newModel);
+		}
+		return models;
 	}
 }
